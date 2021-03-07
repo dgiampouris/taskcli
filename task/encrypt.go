d44a1452312a67e93da7dea2a507c8548c0f0384
Dimitrios Giampouris <d_g@dgiab.org>
2021-03-07 14:21:25 +0100

    Add documentation for each function in encrypt.go and tidy up documentation in general.

--- task/encrypt.go
+++ task/encrypt.go
@@ -13,6 +13,16 @@ import (
 	"syscall"
 )
 
+/*
+   readPassword reads the inputed user password from the cli (stdin).
+   It accepts a bool var that informs the function if this is a new
+   database. If true then the user is prompted to confirm the previously
+   given password. If the confirmation fails, execution ends fatally.
+
+   Implementation details:
+   - The term package is used, specifically the fucntion term.ReadPassword
+     which temporarily changes the prompt and reads a password without echo.
+*/
 func readPassword(newDb bool) []byte {
 	var confirmPass []byte
 
@@ -44,6 +54,13 @@ func readPassword(newDb bool) []byte {
 	return password
 }
 
+/*
+   hashPassword returns a byte slice of the sha256 hash created from
+   the password inputed by the user.
+
+   Implementation details:
+   - It does not salt the password, it just returns the sha256 equivalent hash.
+*/
 func hashPassword(password []byte) []byte {
 	initHash := sha256.Sum256(password)
 	hash := initHash[:]
@@ -51,6 +68,13 @@ func hashPassword(password []byte) []byte {
 	return hash
 }
 
+/*
+   regPassword calls hashPassword and writes the returned key into a a file.
+   It uses SetPaths to get the path where the key is stored.
+
+   Implementation details:
+   - Permissions are set so that only the user can read/write the key file.
+*/
 func regPassword(newDb bool) {
 	var path Path = *SetPaths()
 	key := hashPassword(readPassword(newDb))
@@ -60,7 +84,19 @@ func regPassword(newDb bool) {
 	}
 }
 
-// Should be called with defer in each function that interacts with the db
+/*
+   dbEncrypt encrypts the file where the boltdb database exists.
+   It initialy loads the db into the memory as well as the key.
+   It then creates a new cipher block with a given key of 32 bytes (AES-256).
+   Based on the initial cipher block, a block cipher wrapped in
+   Galois Counter Mode with the standard nonce length is returned.
+   After a nonce is created with the proper length, the data is
+   encrypted by calling Seal and written back to the filesystem
+   at the path provided by path.db.
+
+   Implementation details:
+   - Should be called with defer in each function that interacts with the db.
+*/
 func dbEncrypt() {
 	var path Path = *SetPaths()
 	data, err := os.ReadFile(path.db)
@@ -83,7 +119,9 @@ func dbEncrypt() {
 		log.Panic(err)
 	}
 
-	// Never use more than 2^32 random nonces with a given key
+	// gcm.NonceSize returns the size of the nonce that must be
+	// passed to Seal.
+	// Never use more than 2^32 random nonces with a given key.
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		log.Panic(err)
@@ -97,6 +135,24 @@ func dbEncrypt() {
 	}
 }
 
+/*
+   dbDecrypt decrypts the file where the boltdb database is located.
+   It initialy loads the encrypted file into the memory.
+   Then the key is loaded into the memory, but if it does not exist
+   then regPassword is called. It then creates a new cipher block
+   with a given key of 32 bytes (AES-256). Based on the initial
+   cipher block, a block cipher wrapped in Galois Counter Mode
+   with the standard nonce length is returned.
+   To decrypt, it is necessary to indicate the nonce value used
+   during the encryption process. This value is saved at the
+   beginning of the file. Open decrypts and authenticates
+   the encrypted data.
+
+   Implementation details:
+   - There are 3 total attempts to decrypt the file.
+   - A boolean indicating whether the decryption was successful
+     or not is returned.
+*/
 func dbDecrypt() bool {
 	var path Path = *SetPaths()
 	decSuccess := true
@@ -129,6 +185,8 @@ func dbDecrypt() bool {
 			log.Panic(err)
 		}
 
+		// The nonce value is stored at the beginning of the
+		// encrypted file.
 		nonce := encryptedData[:gcm.NonceSize()]
 		encryptedData = encryptedData[gcm.NonceSize():]
 		data, err := gcm.Open(nil, nonce, encryptedData, nil)
